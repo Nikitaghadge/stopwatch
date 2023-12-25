9df05a8382ac6f4d6318cb5574c9c162ebc07bd4
unknown <ghadgenikita91@gmail.com>
2023-12-25 13:06:44 +0530

    stopwatch

--- src/App.js
+++ src/App.js
@@ -1,46 +1,61 @@
-import { useEffect, React, useState } from "react";
-import "./App.css";
+import React, { useState, useEffect } from 'react';
 
-export default function App() {
-  const [isActivated, setIsActivated] = useState(false);
-  const [timer, setTimer] = useState(0);
+
+const App = () => {
+  const [isRunning, setIsRunning] = useState(false);
+  const [elapsedTime, setElapsedTime] = useState(0);
 
   useEffect(() => {
-    let intervalID;
-
-    if (isActivated) {
-      intervalID = setInterval(() => {
-        setTimer((prevTime) => prevTime + 1);
+    let intervalId;
+    if (isRunning) {
+      intervalId = setInterval(() => {
+        setElapsedTime((prevElapsedTime) => prevElapsedTime + 1);
       }, 1000);
     } else {
-      clearInterval(intervalID);
+      clearInterval(intervalId);
     }
-    return () => clearInterval(intervalID);
-  }, [isActivated]);
+    return () => clearInterval(intervalId);
+  }, [isRunning]);
 
   const startStop = () => {
-    setIsActivated((prevIsActivated) => !prevIsActivated);
+    setIsRunning((prevIsRunning) => !prevIsRunning);
   };
 
   const reset = () => {
-    setIsActivated(false);
-    setTimer(0);
+    setIsRunning(false);
+    setElapsedTime(0);
   };
 
   const formatTime = (seconds) => {
     const minutes = Math.floor(seconds / 60);
     const remainingSeconds = seconds % 60;
-    return `${minutes}:${remainingSeconds < 10 ? "0" : ""}${remainingSeconds}`;
+    return `${minutes}:${remainingSeconds < 10 ? '0' : ''}${remainingSeconds}`;
   };
 
   return (
-    <div className="App">
-      <p>
-        <strong>Stopwatch</strong>
-      </p>
-      <p> Time : {formatTime(timer)}</p>
-      <button onClick={startStop}>{isActivated ? "Stop" : "Start"}</button>
-      <button onClick={reset}>Reset</button>
+    <div className="flex items-center justify-center min-h-screen bg-gray-200">
+      <div className="bg-gray-200 p-6  text-center w-full max-w-md">
+        <h1 className="text-5xl font-bold mb-4 text-indigo-700">Stopwatch</h1>
+        <p className="text-2xl mb-6 text-gray-700"> Time: {formatTime(elapsedTime)}</p>
+        <div className="flex justify-center space-x-4">
+          <button
+            onClick={startStop}
+            className={`${
+              isRunning ? 'bg-red-500' : 'bg-green-500'
+            } text-white px-6 py-3 rounded-md`}
+          >
+            {isRunning ? 'Stop' : 'Start'}
+          </button>
+          <button
+            onClick={reset}
+            className="bg-blue-500 text-white px-6 py-3 rounded-md"
+          >
+            Reset
+          </button>
+        </div>
+      </div>
     </div>
   );
-}
+};
+
+export default App;
